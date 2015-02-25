9368f72e4b1cf8ddca8a6afa064d5c891a616bad
Campbell Allen <campbell.allen@gmail.com>
2015-02-25 19:13:07 +0000

    simple profiling script
    to load workflow data and some power users and the load 250 'normal' users and simulate queries

---
+++ benchmark/load_and_select.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+curl_prefix="http://localhost:4000/workflows/1"
+declare -a power_users=("3685" "9616" "4013" "6538" "1863")
+
+#load subjects for the workflow
+time curl -H 'Accept: application/json' "${curl_prefix}/status"
+time curl -H 'Accept: application/json' "${curl_prefix}/reload"
+
+
+#load all power users seen sets
+for i in "${power_users[@]}"; do
+  time curl -X POST -H 'Accept: application/json' "${curl_prefix}/users/$i/load"
+done
+
+# get some subjects for the power users
+for i in "${power_users[@]}"; do
+  time curl -H 'Accept: application/json' "${curl_prefix}/?user_id=$i"
+done
+
+#load each user in order and select some subjects
+for i in {1..250}; do
+  time curl -X POST -H 'Accept: application/json' "${curl_prefix}/users/$i/load"
+  time curl -H 'Accept: application/json' "${curl_prefix}/?user_id=$i"
+done
+
+# TODO: simulate a peak real workload as it happens, i.e. 10K users hit the site
+#       on day 3 with 5 Mil+ seen subjects for 500K subjects.
+#       If cellect can handle that then we're looking good.
+#       If not what can 1 mode handle and how many nodes do we need for the peak load.
+
+#1. load a workflow for a user
+#2. select some subjects for users.
+#3. update seen subjects for users / simulate classifications coming down
