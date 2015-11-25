d535949904f73d510d70cccad8af0d21fdcb60cc
Campbell Allen <campbell.allen@gmail.com>
2015-11-25 17:15:39 +0000

    docker-compose and create db task

--- benchmark/setup_benchmark_data.sh
+++ benchmark/setup_benchmark_data.sh
@@ -1,9 +1,11 @@
+
 #1. Create the set of data to benchmark with start with (locally or via fig)
 #./benchmark/create_csv_data.rb
-fig run --rm --entrypoint=/bin/bash cellect -c "benchmark/create_csv_data.rb"
+docker-compose run --rm --entrypoint=/bin/bash cellect -c "benchmark/create_csv_data.rb"
 
 #2. create the db schema
-fig run --rm --entrypoint=/bin/bash cellect -c "benchmark/create_db_schema.rb"
+docker-compose run --rm --entrypoint=/bin/bash cellect -c "rake db:create"
+docker-compose run --rm --entrypoint=/bin/bash cellect -c "benchmark/create_db_schema.rb"
 
 #3. load a set of User seens subjects for cellect
-fig run --rm --entrypoint=/bin/bash cellect -c "benchmark/load_csv_data.rb"
+docker-compose run --rm --entrypoint=/bin/bash cellect -c "benchmark/load_csv_data.rb"
