cdd0cf7187594d2101a0ba2e5375e605de718b4c
Campbell Allen <campbell.allen@gmail.com>
2015-11-26 11:05:03 +0000

    update synth data creation scripts
    thanks to https://gist.github.com/parrish/25be85b2fe4d035512d0#file-bootstrap_panoptes-patch

--- benchmark/setup_benchmark_data.sh
+++ benchmark/setup_benchmark_data.sh
@@ -1,11 +1,10 @@
 
 #1. Create the set of data to benchmark with start with (locally or via fig)
-#./benchmark/create_csv_data.rb
-docker-compose run --rm --entrypoint=/bin/bash cellect -c "benchmark/create_csv_data.rb"
+docker-compose run --rm --entrypoint=/bin/bash cellect -c "bundle exec benchmark/create_csv_data.rb"
 
 #2. create the db schema
-docker-compose run --rm --entrypoint=/bin/bash cellect -c "rake db:create"
-docker-compose run --rm --entrypoint=/bin/bash cellect -c "benchmark/create_db_schema.rb"
+docker-compose run --rm --entrypoint=/bin/bash cellect -c "bundle exec rake db:create"
+docker-compose run --rm --entrypoint=/bin/bash cellect -c "bundle exec benchmark/create_db_schema.rb"
 
 #3. load a set of User seens subjects for cellect
-docker-compose run --rm --entrypoint=/bin/bash cellect -c "benchmark/load_csv_data.rb"
+docker-compose run --rm --entrypoint=/bin/bash cellect -c "bundle exec benchmark/load_csv_data.rb"
