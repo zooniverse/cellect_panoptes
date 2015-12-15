a39803962fc1191b6f22b5f771e5e4dd30ed3a63
Campbell Allen <campbell.allen@gmail.com>
2015-12-15 12:31:22 +0000

    more explicit testing instructions

--- benchmark/setup_benchmark_data.sh
+++ benchmark/setup_benchmark_data.sh
@@ -1,5 +1,5 @@
 
-#1. Create the set of data to benchmark with start with (locally or via fig)
+#1. Create the set of data to benchmark to start
 docker-compose run --rm --entrypoint=/bin/bash cellect -c "bundle exec benchmark/create_csv_data.rb"
 
 #2. create the db schema
@@ -7,3 +7,6 @@ docker-compose run --rm --entrypoint=/bin/bash cellect -c "bundle exec rake db:s
 
 #3. load a set of User seens subjects for cellect
 docker-compose run --rm --entrypoint=/bin/bash cellect -c "bundle exec benchmark/load_csv_data.rb"
+
+#4. run some load and selections for users from the service end point, i.e. outside the container via curl
+./benchmark/load_and_select.sh
