93c80cbc1d5dfe69cdb782cf3bfc0585a9ea231e
Campbell Allen <campbell.allen@gmail.com>
2016-07-05 17:30:14 +0100

    load subject data by find each
    ignore duplicate subject -> set records rely on the diffset boost libs to handle any duplicate entries.

--- lib/cellect/server/adapters/panoptes.rb
+++ lib/cellect/server/adapters/panoptes.rb
@@ -61,16 +61,17 @@ module Cellect
               .where(workflows: {id: workflow_id})
               .joins("LEFT OUTER JOIN subject_workflow_counts ON subject_workflow_counts.subject_id = set_member_subjects.subject_id")
               .where('subject_workflow_counts.retired_at IS NULL')
-              .select(:subject_id, :priority, :subject_set_id)
-              .distinct
+              .select(:id, :subject_id, :priority, :subject_set_id)
           end
 
-          subject_data.map do |row|
-            {
-              'id' => row.subject_id,
-              'priority' => 1 / (row.priority || 1).to_f,
-              'group_id' => row.subject_set_id
-            }
+          [].tap do |rows|
+            subject_data.find_each do |s|
+              rows << {
+                'id' => s.subject_id,
+                'priority' => 1 / (s.priority || 1).to_f,
+                'group_id' => s.subject_set_id
+              }
+            end
           end
         end
 
