f247d3bba4829b965fcc19e36e68315db3220bc2
Campbell Allen <campbell.allen@gmail.com>
2016-12-15 16:55:02 +0000

    use the new AR gem configuration options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'bundler'
 RACK_ENV = (ENV['RACK_ENV'] || 'test').to_sym
 Bundler.require(:default, RACK_ENV)
 
+OTR::ActiveRecord.configure_from_url! ENV['DATABASE_URL']
+
+ActiveRecord::Base.logger.level = 1
+
 RSpec.configure do |config|
   config.disable_monkey_patching!
 end
