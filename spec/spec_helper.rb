dffe7628a40e3948b6b5d09dcbb9c3271b408b5e
Campbell Allen <campbell.allen@gmail.com>
2017-12-06 11:13:14 +0000

    run all specs by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,5 @@ ActiveRecord::Base.logger.level = 1
 RSpec.configure do |config|
   config.disable_monkey_patching!
   config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
